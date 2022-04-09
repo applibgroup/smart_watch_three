05ed03b78b5dfa86232e42820a0c53011e3ca5a8
amal2000-cse <kannothparambathamalmanoj@gmail.com>
2022-04-09 14:19:16 +0530

    First commit

---
+++ entry/src/main/java/com/ohos/smart_watch_3/MyApplication.java
@@ -0,0 +1,10 @@
+package com.ohos.smart_watch_3;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
