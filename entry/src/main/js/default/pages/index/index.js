c2dc85ef993cb11bb88a9e15387ea678e13b739c
amal2000-cse <kannothparambathamalmanoj@gmail.com>
2022-05-09 18:53:37 +0530

    "worked in index.js"

--- entry/src/main/js/default/pages/index/index.js
+++ entry/src/main/js/default/pages/index/index.js
@@ -1,6 +1,20 @@
 export default {
     data: {
-        title: ""
+        dot: "common/images/dot.png",
+        partly__sunny:"common/images/partly_sunny.png",
+        partly_sunny:"PARTLY SUNNY",
+        point2:"common/images/dot.png",
+        second:"common/images/seconds_needle.png",
+        centre:"common/images/antique_midpoint.png",
+        hour:"common/images/Hour_needle.png",
+        minute:"common/images/Minutes_needle.png",
+        point3:"common/images/dot.png",
+        calories_img:"common/images/calories_icon.png",
+        foots:"common/images/steps_icon.png",
+        child1:"common/images/dot.png",
+
+
+
     },
     onInit() {
         this.title = this.$t('strings.world');
